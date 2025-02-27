44c19322718518cd1dc925b9acffad95057a14e0
Hanashiko <hlichisper@gmail.com>
2025-02-27 13:52:40 +0200

    fix bug with image

--- backend/app/routes/quest.py
+++ backend/app/routes/quest.py
@@ -252,14 +252,13 @@ def get_user_quests():
 def get_quests_by_user_id(user_id):
     limit = request.args.get('limit', default=10, type=int)
     quests = Quest.query.filter_by(author_id=user_id).order_by(Quest.id.desc()).limit(limit).all()
-    image_url = f"{request.host_url}uploads/{quest.image}" if quest.image else None
     quests_data = [
         {
             "id": quest.id,
             "title": quest.title,
             "description": quest.description,
             "time_limit": quest.time_limit,
-            "image": image_url
+            "image": f"{request.host_url}uploads/{quest.image}" if quest.image else None
         } for quest in quests
     ]
     logger.info(f"Quests retrieved for user {user_id}")
@@ -269,7 +268,6 @@ def get_quests_by_user_id(user_id):
 def get_recent_quests():
     limit = request.args.get('limit', default=10, type=int)
     recent_quests = Quest.query.order_by(Quest.id.desc()).limit(limit).all()
-    # image_url = f"{request.host_url}uploads/{quest.image}" if quest.image else None
     recent_quests_data = [
         {
             "id": quest.id,
@@ -286,9 +284,6 @@ def get_recent_quests():
 @jwt_required()
 def get_all_quests():
     limit = request.args.get('limit',default=10, type=int)
-    # page = request.args.get('page', default=1, type=int)
-    # if page < 1 or limit < 1:
-        # return jsonify({"error":"page and limit must be positive integers"}), 400
     quests = Quest.query.limit(limit).іall()
     if not quests:
         return jsonify({"error":"No quests found for the giver page"}), 404
