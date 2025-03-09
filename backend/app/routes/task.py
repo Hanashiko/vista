b1bf0c6c4ae42a5ad5432300dfae3e169892c206
Hanashiko <hlichisper@gmail.com>
2025-03-09 19:05:29 +0200

    added routes for adding video and images into tasks

--- backend/app/routes/task.py
+++ backend/app/routes/task.py
@@ -1,3 +1,4 @@
+from re import L
 from flask import Blueprint, request, jsonify, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from ..extensions import db, logger
@@ -8,14 +9,11 @@ import os
 
 task_bp = Blueprint('task', __name__)
 
-def allowed_file(filename):
+def allowed_file_image(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
 
-def save_file(file, folder):
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(folder, filename)
-    file.save(file_path)
-    return filename
+def allowed_file_video(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'gif','mp4','avi'}
 
 @task_bp.route('/quests/<int:quest_id>/tasks', methods=['POST'])
 @jwt_required()
@@ -95,3 +93,58 @@ def delete_task(quest_id, task_id):
     db.session.commit()
     logger.info(f"Task {task_id} deleted from quest {quest_id} by user {user_id}")
     return jsonify({"message": "Task deleted successfully"}), 200
+
+@task_bp.route('/quests/<int:quest_id>/tasks/<int:task_id>/image', methods=['POST'])
+@jwt_required()
+def upload_task_image(quest_id, task_id):
+    user_id = get_jwt_identity()
+    quest = Quest.query.get_or_404(quest_id)
+    task = Task.query.get_or_404(task_id)
+
+    if int(quest.author_id) != int(user_id):
+        return jsonify({"message":"You are not authorized to upload an image for this task"}), 403
+
+    if 'image' not in request.files:
+        return jsonify({"message":"No image file provided"}), 400
+
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({"message":"No selected file"}), 400
+
+    if file and allowed_file_image(file.filename):
+        filename = f"quest_{quest_id}_task_{task_id}_{file.filename}"
+        file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        task.image = filename
+        db.session.commit()
+        logger.info(f"Image updated for task: {task.id} from quest: {quest.id} - {quest.title}")
+        return jsonify({"message":"Image of task updated successfully","image_url":f"{request.host_url}uploads/{filename}"}),200
+    return jsonify({"message":"Invalid file type"}),400
+
+@task_bp.route('/quests/<int:quest_id>/tasks/<int:task_id>/video', methods=['POST'])
+@jwt_required()
+def uploads_task_video(quest_id, task_id):
+    user_id = get_jwt_identity()
+    quest = Quest.query.get_or_404(quest_id)
+    task = Task.query.get_or_404(task_id)
+
+    if int(quest.author_id) != int(user_id):
+        return jsonify({"message":"You are not authorized to upload an video for this task"}), 403
+    
+    if 'video' not in request.files:
+        return jsonify({"message":"No video file provided"}), 400
+
+    file = request.files['video']
+    if file.filename == '':
+        return jsonify({"message":"No selected file"}), 400
+
+    if file and allowed_file_video(file.filename):
+        filename = f"quest_{quest_id}_task_{task_id}_{file.filename}"
+        file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        task.video = filename
+        db.session.commit()
+        logger.info(f"Video updated for task: {task.id} from quest: {quest.id} - {quest.title}")
+        return jsonify({"message":"Video of task updated successfully","image_url":f"{request.host_url}uploads/{filename}"})
+    return jsonify({"message":"Invalid file type"}),400
+        
