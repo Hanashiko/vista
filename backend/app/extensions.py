44060a7f2f67b4620c2d4ade5058da4a5e192e5a
Hanashiko <hlichisper@gmail.com>
2025-06-22 01:32:20 +0300

    fix: changed order of imports in extensions

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,10 +1,12 @@
+import logging
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
-import logging
 from flasgger import Swagger
+from .models import User
+from .models import RevokedToken
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,13 +20,10 @@ swagger = Swagger()
 
 @login_manager.user_loader
 def load_user(user_id):
-    from .models import User
     return User.query.get(int(user_id))
 
 @jwt.token_in_blocklist_loader
 def check_if_token_revoked(jwt_header, jwt_payload):
-    from .models import RevokedToken
     jti = jwt_payload["jti"]
     token = RevokedToken.query.filter_by(jti=jti).first()
     return token is not None
-
