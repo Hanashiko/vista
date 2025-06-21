b61f7fe86119f49d1e7f60113061849825173d8a
Hanashiko <hlichisper@gmail.com>
2025-06-22 01:25:33 +0300

    fix: removed garbage lines of commented code

--- backend/app/decorators.py
+++ backend/app/decorators.py
@@ -10,10 +10,6 @@ def requires_secret_key(required_value: str):
             key = request.headers.get('X-Secret-Key') or request.args.get('secret_key')
             if key != Config.ADMIN_SECRET_KEY:
                 abort(403, description="Forbidden: invalid secret key")
-            # verify_jwt_in_request()
-            # claims = get_jwt()
-            # if 'key' not in claims or claims['key'] != required_value:
-            #     return jsonify({'error': 'Access denied: invalid secret_key'}), 403
             return fn(*args, **kwargs)
         return decorator
     return wrapper
